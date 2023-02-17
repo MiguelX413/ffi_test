dfd45cc2ff035a756c14656df98a47f52c4c1e9a
MiguelX413 <lbpgaming27@gmail.com>
2023-02-16 23:59:02 -0800

    Add warning suppressions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::ffi::CStr;
 
 mod bindings {
+    #![allow(dead_code)]
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
